10bd02612ea5992b1f964a08d457608babb1f2e3
KenNyagz <kennyagz681@gmail.com>
2024-07-05 12:39:44 +0300

    aliased commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -53,7 +53,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> connection:
+def get_db() -> mysql.connector.connection.MySQLConnection:
     '''connects to a database and returns the connection object'''
     db_user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
     db_password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
