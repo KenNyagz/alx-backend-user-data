f35d90beb0bd63cf2949243f163fbf0463dcf8f6
KenNyagz <kennyagz681@gmail.com>
2024-07-05 12:32:30 +0300

    aliased commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -27,8 +27,10 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord) -> str:
         '''method that handles the log formatting'''
-        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
-        return super(RedactingFormatter, self).format(record)
+        message = super(RedactingFormatter, self).format(record)
+        record_msg = filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
+        return record_msg
+        # return super(RedactingFormatter, self).format(record)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
