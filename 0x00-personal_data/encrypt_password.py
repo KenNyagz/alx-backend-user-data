15889722fd10581e37072b95468841101c391129
KenNyagz <kennyagz681@gmail.com>
2024-07-05 19:35:32 +0300

    aliased commit

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,7 +12,4 @@ def hash_password(password: str) -> bytes:
 
 def is_valid(encrpd_pw: bytes, password: str) -> bool:
     '''checks if the password entered is correct'''
-    if bcrypt.checkpw(password.encode(), encrpd_pw):
-        return True
-    else:
-        return False
+    return bcrypt.checkpw(password.encode(), encrpd_pw):
