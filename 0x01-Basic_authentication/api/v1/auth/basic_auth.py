1195ac36ce7816dd6fc9ca3a764c085528138c30
KenNyagz <kennyagz681@gmail.com>
2024-07-10 05:38:12 +0300

    find user by email

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 basic authentication
 '''
 import base64
+from typing import TypeVar
+from models.user import User
 from api.v1.auth.auth import Auth
 
 
@@ -42,3 +44,18 @@ class BasicAuth(Auth):
 
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        '''returns the User instance based on his email and password'''
+        if not user_email or not isinstance(user_email, str):
+            return None
+        if not user_pwd or not isinstance(user_pwd, str):
+            return None
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+        user = users[0]
+
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
