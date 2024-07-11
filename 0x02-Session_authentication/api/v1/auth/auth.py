480f218f3cb9fb13632212d410f17b58bf906179
KenNyagz <kennyagz681@gmail.com>
2024-07-11 18:21:48 +0300

    session cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -44,6 +44,6 @@ class Auth:
         '''returns a cookie value from a request'''
         if request is None:
             return None
-     
+
         session_name = os.getenv('SESSION_NAME', '_my_session_id')
         return request.cookies.get(session_name)
