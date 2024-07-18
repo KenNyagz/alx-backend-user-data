6cb77ab224e1ae3604e142b95130064825d0e2fb
KenNyagz <kennyagz681@gmail.com>
2024-07-18 19:01:53 +0300

    a flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+return a JSON payload of the form:
+
+{"message": "Bienvenue"}
+'''
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+app.route('/')
+def payload():
+    '''returns a JSON payload of form'''
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port="5000")
