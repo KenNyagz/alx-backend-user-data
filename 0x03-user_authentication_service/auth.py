e15e51c1c154602f7f993c57b8d0d5ca970d1480
KenNyagz <kennyagz681@gmail.com>
2024-07-18 21:58:00 +0300

    validating credentials

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -38,7 +38,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             return bcrypt.checkpw(password.encode('utf-8'),
-                                  user.hashed_password('utf-8'))
+                                  user.hashed_password)
         except NoResultFound:
             return False
         except Exception:
