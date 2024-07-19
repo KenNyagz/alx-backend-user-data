2b7eb97bf1ee5e733b035d5abe3b935e43cece4d
KenNyagz <kennyagz681@gmail.com>
2024-07-19 09:53:28 +0300

    log in

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -44,7 +44,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             if user and bcrypt.checkpw(password.encode('utf-8'),
-                                       user.hashed_password.encode('utf-8')):
+                                       user.hashed_password):
                 return True
             else:
                 return False
