f873e6b7c27efc73154b0de07fcbf0e3103f49b7
KenNyagz <kennyagz681@gmail.com>
2024-07-19 09:43:14 +0300

    log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,7 +4,7 @@ return a JSON payload of the form:
 
 {"message": "Bienvenue"}
 '''
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 app = Flask(__name__)
@@ -31,6 +31,27 @@ def users():
         return jsonify({"email": user.email, "message": "user created"}), 201
     except ValueError as e:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    '''handles user sessions'''
+    email = request.form.get('email')
+    password =  request.form.get('password')
+
+    if not email or not password:
+        abort(401)
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    if not session_id:
+        abort(401)
+
+    response = make_response(jsonify({"email": email, "message": "logged in"}))
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == '__main__':
